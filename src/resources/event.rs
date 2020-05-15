7e6caf3dddd1413057814640e4b8b043b04c1cb6
Roman Useinov <roman.useinov@gmail.com>
2020-05-15 17:07:35 +0200

    Fixes for Invoices
     - fix invoiceLineItemID
     - add card
     - add paymentMethod
     - modify paymentMethodId
     - add     payment_method.attached
     - add payment_intent.succeeded
     - fix invoiceLineItemId in webhooks + not in webhooks

--- src/resources/event.rs
+++ src/resources/event.rs
@@ -138,6 +138,8 @@ pub enum EventType {
     PaymentIntentRequiresCapture,
     #[serde(rename = "payment_intent.succeeded")]
     PaymentIntentSucceeded,
+    #[serde(rename = "payment_method.attached")]
+    PaymentMethodAttached,
     #[serde(rename = "payout.canceled")]
     PayoutCanceled,
     #[serde(rename = "payout.created")]
@@ -212,6 +214,7 @@ pub enum EventObject {
     ApplicationFeeRefund(ApplicationFeeRefund),
     Balance(Balance),
     BankAccount(BankAccount),
+    Card(Card),
     Charge(Charge),
     Customer(Customer),
     Dispute(Dispute),
@@ -223,6 +226,7 @@ pub enum EventObject {
     InvoiceItem(InvoiceItem),
     Order(Order),
     OrderReturn(OrderReturn),
+    PaymentMethod(PaymentMethod),
     PaymentIntent(PaymentIntent),
     Payout(Payout),
     Plan(Plan),
