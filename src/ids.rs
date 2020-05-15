756ee4df6bb89de79c24e18ce4dfc34488a4c101
Roman Useinov <roman.useinov@gmail.com>
2020-05-15 17:07:35 +0200

    fix invoiceLineItemID

--- src/ids.rs
+++ src/ids.rs
@@ -492,12 +492,14 @@ def_id!(FileId, "file_");
 def_id!(FileLinkId, "link_");
 def_id!(InvoiceId, "in_", { _ });
 def_id!(InvoiceItemId, "ii_");
-def_id!(
-    enum InvoiceLineItemId {
-        Item(InvoiceItemId),
-        Subscription(SubscriptionLineId),
-    }
-);
+def_id!(InvoiceLineItemId, "il_");
+
+// def_id!(
+//     enum InvoiceLineItemId {
+//         Item(InvoiceItemId),
+//         Subscription(SubscriptionLineId),
+//     }
+// );
 def_id!(IssuingAuthorizationId, "iauth_");
 def_id!(IssuingCardId, "ic_");
 def_id!(IssuingCardholderId, "ich_");
