23c8e2d49986f6ead69496283fb07367d0e438e3
phayes <patrick.d.hayes@gmail.com>
2021-06-03 10:07:44 -0700

    Adding usage record summaries support

--- src/resources/subscription_item_ext.rs
+++ src/resources/subscription_item_ext.rs
@@ -1,6 +1,7 @@
 use crate::config::{Client, Response};
-use crate::ids::{SubscriptionItemId, UsageRecordId};
-use crate::params::Timestamp;
+use crate::ids::{InvoiceItemId, SubscriptionItemId, UsageRecordId, UsageRecordSummaryId};
+use crate::params::{Expand, List, Timestamp};
+use crate::Period;
 use serde_derive::{Deserialize, Serialize};
 
 /// The resource representing a Stripe "UsageRecord".
@@ -67,5 +68,87 @@ impl UsageRecord {
         params: CreateUsageRecord,
     ) -> Response<Self> {
         client.post_form(&format!("/subscription_items/{}/usage_records", id), &params)
+    }
+}
+
+/// The resource representing a Stripe "UsageRecordSummary".
+///
+/// For more details see [https://stripe.com/docs/api/usage_records/subscription_item_summary_list](https://stripe.com/docs/api/usage_records/subscription_item_summary_list).
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct UsageRecordSummary {
+    /// Unique identifier for the object.
+    pub id: UsageRecordSummaryId,
+
+    /// The invoice in which this usage period has been billed for.
+    pub invoice: Option<InvoiceItemId>,
+
+    // The usage period.
+    pub period: Period,
+
+    /// The ID of the subscription item this summary is describing.
+    pub subscription_item: SubscriptionItemId,
+
+    /// The total usage within this usage period.
+    pub total_usage: u64,
+}
+
+impl UsageRecordSummary {
+    /// For the specified subscription item, returns a list of summary objects. Each object in the list provides usage information that’s been summarized from multiple usage records and over a subscription billing period (e.g., 15 usage records in the month of September).
+    ///
+    /// The list is sorted in reverse-chronological order (newest first). The first list item represents the most current usage period that hasn’t ended yet. Since new usage records can still be added, the returned summary information for the subscription item’s ID should be seen as unstable until the subscription billing period ends.
+    pub fn list(
+        client: &Client,
+        params: ListUsageRecordSummaries<'_>,
+    ) -> Response<List<UsageRecordSummary>> {
+        client.get_query(
+            &format!("/subscription_items/{}/usage_record_summaries", params.subscription_item),
+            &params,
+        )
+    }
+}
+
+/// For the specified subscription item, returns a list of summary objects. Each object in the list provides usage information that’s been summarized from multiple usage records and over a subscription billing period (e.g., 15 usage records in the month of September).
+///
+/// The list is sorted in reverse-chronological order (newest first). The first list item represents the most current usage period that hasn’t ended yet. Since new usage records can still be added, the returned summary information for the subscription item’s ID should be seen as unstable until the subscription billing period ends.
+/// For more details see [https://stripe.com/docs/api/usage_records/subscription_item_summary_list](https://stripe.com/docs/api/usage_records/subscription_item_summary_list).
+#[derive(Clone, Debug, Serialize)]
+pub struct ListUsageRecordSummaries<'a> {
+    /// A cursor for use in pagination.
+    ///
+    /// `ending_before` is an object ID that defines your place in the list.
+    /// For instance, if you make a list request and receive 100 objects, starting with `obj_bar`, your subsequent call can include `ending_before=obj_bar` in order to fetch the previous page of the list.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ending_before: Option<UsageRecordSummaryId>,
+
+    /// Specifies which fields in the response should be expanded.
+    #[serde(skip_serializing_if = "Expand::is_empty")]
+    pub expand: &'a [&'a str],
+
+    /// A limit on the number of objects to be returned.
+    ///
+    /// Limit can range between 1 and 100, and the default is 10.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub limit: Option<u64>,
+
+    /// A cursor for use in pagination.
+    ///
+    /// `starting_after` is an object ID that defines your place in the list.
+    /// For instance, if you make a list request and receive 100 objects, ending with `obj_foo`, your subsequent call can include `starting_after=obj_foo` in order to fetch the next page of the list.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub starting_after: Option<UsageRecordSummaryId>,
+
+    /// The ID of the subscription whose usage record summaries will be retrieved.
+    pub subscription_item: SubscriptionItemId,
+}
+
+impl<'a> ListUsageRecordSummaries<'a> {
+    pub fn new(subscription_item: SubscriptionItemId) -> Self {
+        ListUsageRecordSummaries {
+            ending_before: Default::default(),
+            expand: Default::default(),
+            limit: Default::default(),
+            starting_after: Default::default(),
+            subscription_item,
+        }
     }
 }
